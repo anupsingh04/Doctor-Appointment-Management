bc39e844e1fd5dc23ed7083515a213c6998eed60
anupsingh04 <anupsingh727716@gmail.com>
2025-09-02 03:02:58 +0530

    Initial Commit and Normalize line endings

---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Appointment from "./Pages/Appointment";
+import AboutUs from "./Pages/AboutUs";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Context } from "./main";
+
+// console.log("API URL FROM ENV:", import.meta.env.VITE_API_URL);
+// const API = axios.create({ baseURL: process.env.REACT_APP_API_URL }); // Doesn't work for vite
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+
+const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await API.get("/user/patient/me", {
+          withCredentials: true,
+        });
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+        <ToastContainer position="top-center" />
+      </Router>
+    </>
+  );
+};
+
+export default App;
